63148c5859347bab925a286a191bc23c6d7f54fd
Robert Mensing <rmensing@gmail.com>
2016-03-03 15:53:18 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
+'use strict';
 var PORT = process.env.PORT || 5000;
 var express = require("express");
 var snoowrap = require('snoowrap');
+r = new snoowrap(require('../config.json'))
  
 var app = express();
 app.use(app.router);
 app.use(express.static(__dirname + "/public"));
  
 app.get("/", function(req, res){
+ // Printing a list of the titles on the front page
+   r.get_hot().map(post => post.title).then(console.log);
     res.send("Hello, World!");
 });
  
